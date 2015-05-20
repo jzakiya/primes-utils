2ad2215e8b1818b5c4f404dac914b33505775c9d
Jabari Zakiya <jzakiya@gmail.com>
2015-05-19 21:19:00 -0400

    first code commit

---
+++ lib/primes/utils.rb
@@ -0,0 +1,7 @@
+require "primes/utils/version"
+
+module Primes
+  module Utils
+    # Your code goes here...
+  end
+end
---
+++ lib/primes/utils/version.rb
@@ -0,0 +1,5 @@
+module Primes
+  module Utils
+    VERSION = "0.1.0"
+  end
+end
